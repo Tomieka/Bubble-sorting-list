b3d564520ab4e8b799d1e6f1f401f94d4126342b
Tomieka <89526666+Tomieka@users.noreply.github.com>
2023-12-09 20:15:33 +0100

    Add files via upload

---
+++ SortowanieMetodaBobelkowa.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n-1; i++) {
+        for (int j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int arr[] = {3,7,4,8,36,94,4,8,2,7,2};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    cout << "Oryginalna lista: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    bubbleSort(arr, n);
+
+    cout << "\nWysortowana lista: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
+
+// Program sortujący listę metodą bombelkową.
